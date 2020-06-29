21501bd3da85da22238639e048ff089b5d384418
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-06-29 14:29:26 -0300

    powershell command added[0]

--- ccpp/__main__.py
+++ ccpp/__main__.py
@@ -30,26 +30,53 @@ def run():
     # Get compiled name of the target file
     divisions = args.target_file.split('.')
     ext = divisions[len(divisions) - 1]
-    compiled_name = 'bin_' + args.target_file.replace('.' + ext, '')
+    compiled_name = args.target_file.replace('.' + ext, '')
 
     # Get all g++ args
     gpp_args = ''
     for arg in unknown:
         gpp_args += arg + ' '
 
-    # Get the run command string
-    command = "g++ {} -o {} ".format(args.target_file,
-                                     compiled_name) + gpp_args
-    command += "&& ./{} ".format(compiled_name)
-    if not args.noremove:
-        command += "&& rm -rf {}".format(compiled_name)
+    command = ''
+    clip_command = ''
 
-    # Run command
+    # On Windows
+    if os.name == 'nt':
+
+        # Get the run command string
+        command += "powershell.exe g++ {} -o {} ; ".format(args.target_file,
+                                                           compiled_name) + gpp_args
+
+        command += "{}.exe ; ".format(compiled_name)
+
+        if not args.noremove:
+            command += "rm -r {}.exe".format(compiled_name)
+
+        # If "-c" or "--clip" exists, then run
+        if args.clip:
+            clip_command += "powershell.exe Get-Content {} | clip".format(
+                args.target_file)
+
+    # On Unix
+    else:
+
+        # Get the run command string
+        command += "g++ {} -o {} && ".format(args.target_file,
+                                             compiled_name) + gpp_args
+
+        command += "./{} && ".format(compiled_name)
+
+        if not args.noremove:
+            command += "rm -rf {}".format(compiled_name)
+
+        # If "-c" or "--clip" exists, then run
+        if args.clip:
+            clip_command += "xclip -selection clipboard {}".format(
+                args.target_file)
+
     os.system(command)
-
-    # If "-c" or "--clip" exists, then run
     if args.clip:
-        os.system(f"xclip -selection clipboard {args.target_file}")
+        os.system(clip_command)
 
 
 if __name__ == "__main__":
